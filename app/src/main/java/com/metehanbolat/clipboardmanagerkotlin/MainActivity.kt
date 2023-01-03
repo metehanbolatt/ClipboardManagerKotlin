b15f1528c4892d9136fced97ca98ef3a7673ac9a
Metehan <metehan.bolat.ie@gmail.com>
2023-01-03 19:22:30 +0300

    ClipBoardManager was added.

--- app/src/main/java/com/metehanbolat/clipboardmanagerkotlin/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/clipboardmanagerkotlin/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.metehanbolat.clipboardmanagerkotlin
 
+import android.content.ClipDescription.MIMETYPE_TEXT_PLAIN
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.metehanbolat.clipboardmanagerkotlin.databinding.ActivityMainBinding
@@ -13,6 +16,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        //val clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+        val clipboard2 = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+
+        binding.button.setOnClickListener {
+            //println("text: ${clipboard.setPrimaryClip(ClipData.newPlainText("Deneme", "Text"))}")
+            if (clipboard2.primaryClipDescription!!.hasMimeType(MIMETYPE_TEXT_PLAIN)) {
+                val item = clipboard2.primaryClip!!.getItemAt(0)
+                val yourText = item.text.toString()
+                binding.textView.text = "MyText: $yourText"
+                println("My text: $yourText")
+            }
+        }
 
     }
 }
