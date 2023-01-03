87bb87e94fc81305c78276265aae827d88e197e6
Metehan <metehan.bolat.ie@gmail.com>
2023-01-03 19:21:55 +0300

    Initial commit

---
+++ app/src/main/java/com/metehanbolat/clipboardmanagerkotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.metehanbolat.clipboardmanagerkotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.metehanbolat.clipboardmanagerkotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+    }
+}
